6d8714f8a0400c4720b61a8e9df391c86f4f6654
hudsonviana <hudson1206@gmail.com>
2024-05-04 18:27:02 -0300

    Ep 2.3 - Loading A Movie Using Route Parameters

--- start/routes.ts
+++ start/routes.ts
@@ -1,9 +1,14 @@
 import router from '@adonisjs/core/services/router'
+import app from '@adonisjs/core/services/app'
+import fs from 'node:fs/promises'
 
 router.on('/').render('pages/home').as('home')
 
 router
-  .get('/movies/my-awesome-movie', async (ctx) => {
-    return ctx.view.render('pages/movies', { movie: 'TESTE Meu filme incrível!' })
+  .get('/movies/:slug', async (ctx) => {
+    const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
+    const movie = await fs.readFile(url, 'utf-8')
+
+    return ctx.view.render('pages/movies/show', { movie })
   })
   .as('movies.show')
