ab8d77ee78a715a709cdefb6c8e60a72d1c9200a
hudsonviana <hudson1206@gmail.com>
2024-05-04 17:46:19 -0300

    Ep 2.2 - Linking Between Routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,7 +1,9 @@
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home').as('home')
 
-router.get('/movies', async (ctx) => {
-  return ctx.view.render('pages/movies', { movie: 'TESTE Meu filme incrível!' })
-})
+router
+  .get('/movies/my-awesome-movie', async (ctx) => {
+    return ctx.view.render('pages/movies', { movie: 'TESTE Meu filme incrível!' })
+  })
+  .as('movies.show')
