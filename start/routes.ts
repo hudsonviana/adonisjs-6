b954fca79f86c60919cd4aefe8211d31badd6e52
hudsonviana <hudson1206@gmail.com>
2024-05-04 17:08:45 -0300

    Let's Learn AdonisJS 6: Ep 2.0 - Routes and How To Create Them

--- start/routes.ts
+++ start/routes.ts
@@ -1,12 +1,7 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.get('/teste', async (ctx) => {
+  return 'Mas que nada, um samba como esse...'
+})
