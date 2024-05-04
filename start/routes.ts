53c4d6ce368487e67dbf6a0b6bc5b9fb2646f730
hudsonviana <hudson1206@gmail.com>
2024-05-04 18:56:04 -0300

    Ep 2.4 - Validating Route Parameters

--- start/routes.ts
+++ start/routes.ts
@@ -1,14 +1,26 @@
 import router from '@adonisjs/core/services/router'
 import app from '@adonisjs/core/services/app'
 import fs from 'node:fs/promises'
+import { Exception } from '@adonisjs/core/exceptions'
 
 router.on('/').render('pages/home').as('home')
 
 router
   .get('/movies/:slug', async (ctx) => {
     const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
-    const movie = await fs.readFile(url, 'utf-8')
+    let movie: any
 
-    return ctx.view.render('pages/movies/show', { movie })
+    try {
+      movie = await fs.readFile(url, 'utf-8')
+      ctx.view.share({ movie })
+    } catch (error) {
+      throw new Exception(`Could not find a movie called ${ctx.params.slug}`, {
+        code: 'E_NOT_FOUND',
+        status: 404,
+      })
+    }
+
+    return ctx.view.render('pages/movies/show')
   })
   .as('movies.show')
+  .where('slug', router.matchers.slug())
