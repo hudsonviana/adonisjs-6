8c30c3bcf9b48d7345dc449cc7c7d22e03eebd7f
hudsonviana <hudson1206@gmail.com>
2024-05-05 23:13:39 -0300

    Ep 2.10 - Cleaning Up Routes with Controllers

--- start/routes.ts
+++ start/routes.ts
@@ -1,33 +1,9 @@
 import router from '@adonisjs/core/services/router'
-import { toHtml } from '@dimerapp/markdown/utils'
-import MovieService from '#services/movie_service'
+import MoviesController from '#controllers/movies_controller'
+
+router.get('/', [MoviesController, 'index']).as('home')
 
 router
-  .get('/', async (ctx) => {
-    const slugs = await MovieService.getSlugs()
-    const movies: Record<string, any>[] = []
-
-    for (const slug of slugs) {
-      const md = await MovieService.read(slug)
-
-      movies.push({
-        title: md.frontmatter.title,
-        summary: md.frontmatter.summary,
-        slug,
-      })
-    }
-
-    return ctx.view.render('pages/home', { movies })
-  })
-  .as('home')
-
-router
-  .get('/movies/:slug', async (ctx) => {
-    const md = await MovieService.read(ctx.params.slug)
-    const movie = toHtml(md).contents
-    ctx.view.share({ movie, md })
-
-    return ctx.view.render('pages/movies/show')
-  })
+  .get('/movies/:slug', [MoviesController, 'show'])
   .as('movies.show')
   .where('slug', router.matchers.slug())
