bc959d4942641c5c76ef70d470b8ea857f27fc86
hudsonviana <hudson1206@gmail.com>
2024-05-11 23:38:49 -0300

    Ep 2.15 - Deleting Items and Flushing our Redis Cache

--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,6 @@
 import router from '@adonisjs/core/services/router'
 import MoviesController from '#controllers/movies_controller'
+import RedisController from '#controllers/redis_controller'
 
 router.get('/', [MoviesController, 'index']).as('home')
 
@@ -7,3 +8,6 @@ router
   .get('/movies/:slug', [MoviesController, 'show'])
   .as('movies.show')
   .where('slug', router.matchers.slug())
+
+router.get('/redis/flush', [RedisController, 'flush']).as('redis.flush')
+router.get('/redis/:slug', [RedisController, 'destroy']).as('redis.destroy')
