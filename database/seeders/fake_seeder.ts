635c87bcc6d933189e25f757bd762f6f4d0c81bb
hudsonviana <hudson1206@gmail.com>
2024-05-19 23:43:37 -0300

    Ep 4.8 - Stubbing Fake Data with Model Factories

---
+++ database/seeders/fake_seeder.ts
@@ -0,0 +1,16 @@
+import { CineastFactory } from '#database/factories/cineast_factory'
+import { MovieFactory } from '#database/factories/movie_factory'
+import { UserFactory } from '#database/factories/user_factory'
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+
+export default class extends BaseSeeder {
+  static environment: ['development']
+
+  async run() {
+    // Write your database queries inside the run method
+
+    await CineastFactory.createMany(10)
+    await MovieFactory.createMany(3)
+    await UserFactory.createMany(5)
+  }
+}
