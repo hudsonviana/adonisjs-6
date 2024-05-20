eb84b8cdb7419fa5ce58d7e688bb31e2bce03731
hudsonviana <hudson1206@gmail.com>
2024-05-19 21:27:39 -0300

    Ep 4.6 - The Basics of CRUD

--- app/controllers/movies_controller.ts
+++ app/controllers/movies_controller.ts
@@ -1,19 +1,14 @@
 import Movie from '#models/movie'
-// import cache from '#services/cache_service'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class MoviesController {
   async index({ view }: HttpContext) {
     const movies = await Movie.all()
     return view.render('pages/home', { movies })
-
-    // return view.render('pages/home')
   }
 
   async show({ view, params }: HttpContext) {
-    // const movie = cache.get(params.slug)
-    const movie = await Movie.find(params.slug)
-
+    const movie = await Movie.findByOrFail('slug', params.slug)
     return view.render('pages/movies/show', { movie })
   }
 }
